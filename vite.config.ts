ff6b8820085cda72101ad716282bf55cc5f22d6f
kheang.hc <kheang.hc@prosomo.com>
2024-10-04 07:02:55 -0400

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
 import { UserConfig } from 'vite'
+import vikeNode from 'vike-node/plugin'
 
 const config: UserConfig = {
-  plugins: [react(), vike()]
+  plugins: [react(), vikeNode({
+    entry: 'server/index.ts',
+    standalone: true
+  }), vike()]
 }
 
 export default config
